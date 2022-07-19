a34ae88bd871d64aa5c33a5a703b2607b4a6f7df
Joel Conraud <Joel.Conraud@emerson.com>
2022-07-19 18:17:07 +0200

    Do not go through strava login / authorization anymore if token, including refresh one, is still valide

--- backend/app.py
+++ backend/app.py
@@ -18,41 +18,10 @@ app.config.from_envvar('APP_SETTINGS')
 
 @app.route("/")
 def login():
-    c = Client()
-    url = c.authorization_url(client_id=app.config['STRAVA_CLIENT_ID'],
-                              redirect_uri=url_for('.logged_in',
-                              _external=True),
-                              approval_prompt='auto',
-                              scope=['read_all','activity:read_all','activity:write'])
-    return render_template('login.html', authorize_url=url)
-
-
-@app.route("/strava-oauth")
-def logged_in():
-    """
-    Method called by Strava (redirect) that includes parameters.
-    - state
-    - code
-    - error
-    """
-    error = request.args.get('error')
-    state = request.args.get('state')
-    if error:
-        return render_template('login_error.html', error=error)
-    else:
-        code = request.args.get('code')
-        client = Client()
-        
-        if os.path.exists("stravaexp.dat"):
-            with open("stravaexp.dat", mode="r") as data_file:
-                access_token = json.load(data_file)
-        else:
-            access_token = client.exchange_code_for_token(client_id=app.config['STRAVA_CLIENT_ID'],
-                                                      client_secret=app.config['STRAVA_CLIENT_SECRET'],
-                                                      code=code)
-            with open('stravaexp.dat', 'w') as f:
-                json.dump(access_token, f)
-        
+    client = Client()
+    if os.path.exists("stravaexp.dat"):
+        with open("stravaexp.dat", mode="r") as data_file:
+            access_token = json.load(data_file)
         if time.time() > access_token['expires_at']:
             print('Token has expired, will refresh')
             refresh_response = client.refresh_access_token(
@@ -72,8 +41,46 @@ def logged_in():
             client.access_token = access_token['access_token']
             client.refresh_token = access_token['refresh_token']
             client.token_expires_at = access_token['expires_at']
-                
-        # Probably here you'd want to store this somewhere -- e.g. in a database.
+
+        strava_athlete = client.get_athlete()
+        processed = processactivities.process_activities(client)
+
+        return render_template(
+            'login_results.html',
+            athlete=strava_athlete,
+            access_token=access_token,
+            processed=processed)
+    else:                
+        url = client.authorization_url(client_id=app.config['STRAVA_CLIENT_ID'],
+                                redirect_uri=url_for('.logged_in',
+                                _external=True),
+                                approval_prompt='auto',
+                                scope=['read_all','activity:read_all','activity:write'])
+        return render_template('login.html', authorize_url=url)
+
+
+@app.route("/strava-oauth")
+def logged_in():
+    """
+    Method called by Strava (redirect) that includes parameters.
+    - state
+    - code
+    - error
+    """
+    error = request.args.get('error')
+    state = request.args.get('state')
+    if error:
+        return render_template('login_error.html', error=error)
+    else:
+        code = request.args.get('code')
+        client = Client()
+        
+        access_token = client.exchange_code_for_token(client_id=app.config['STRAVA_CLIENT_ID'],
+                                                    client_secret=app.config['STRAVA_CLIENT_SECRET'],
+                                                    code=code)
+        with open('stravaexp.dat', 'w') as f:
+            json.dump(access_token, f)
+        
         strava_athlete = client.get_athlete()
         processed = processactivities.process_activities(client)
 
